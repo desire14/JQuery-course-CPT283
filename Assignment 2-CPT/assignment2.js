670e1d95650eb688fe3ff64f051e316e9dcdd4a2
Dasaray Fyall <51466820+desire14@users.noreply.github.com>
2022-01-26 21:24:26 -0500

    Add files via upload

---
+++ Assignment 2-CPT/assignment2.js
@@ -0,0 +1,23 @@
+function area(height,weight) {
+    var area = height * weight
+    return area;
+}
+var alphabet = new Array();
+for (var i = 0; i !=26; ++i)
+{
+    
+    alphabet.push 
+    (
+        String.fromCharCode(i + 65)
+    );
+}
+
+$(document).ready(function() {
+    $("#letters") .addClass("Christmas");
+
+});
+
+$(document).ready(function() {
+    $("#calculation") .addClass("Easter");
+
+});
